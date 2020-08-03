c317e518e57f7fc3fd01aa5d8b09b1f86a949233
hd whitefur <hughdwhite@gmail.com>
2020-08-03 17:41:52 -0500

    create helper file Presets.js with presets accessible in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as presets from "./Presets";
 import Nav from "./Nav";
 import Grid from "./Grid";
 import Control from "./Control";
@@ -8,24 +9,7 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			emoji: [
-				{ emoji: "⬛️", distance: { min: 0, max: 0 }, angle: null },
-				{ emoji: "◼️", distance: { min: 0, max: 1 }, angle: null },
-				{ emoji: "▪️", distance: { min: 0, max: 2 }, angle: null },
-				{ emoji: "👈", distance: { min: 3, max: 5 }, angle: { gt: 337.5, lt: 22.5 } },
-				{ emoji: "👇", distance: { min: 3, max: 5 }, angle: { gt: 67.5, lt: 112.5 } },
-				{ emoji: "👉", distance: { min: 3, max: 5 }, angle: { gt: 157.5, lt: 202.5 } },
-				{ emoji: "👆", distance: { min: 3, max: 5 }, angle: { gt: 247.5, lt: 292.5 } },
-				{ emoji: "⬅️", distance: { min: 6, max: 8 }, angle: { gt: 337.5, lt: 22.5 } },
-				{ emoji: "↙️", distance: { min: 6, max: 8 }, angle: { gt: 22.5, lt: 67.5 } },
-				{ emoji: "⬇️", distance: { min: 6, max: 8 }, angle: { gt: 67.5, lt: 112.5 } },
-				{ emoji: "↘️", distance: { min: 6, max: 8 }, angle: { gt: 112.5, lt: 157.5 } },
-				{ emoji: "➡️", distance: { min: 6, max: 8 }, angle: { gt: 157.5, lt: 202.5 } },
-				{ emoji: "↗️", distance: { min: 6, max: 8 }, angle: { gt: 202.5, lt: 247.5 } },
-				{ emoji: "⬆️", distance: { min: 6, max: 8 }, angle: { gt: 247.5, lt: 292.5 } },
-				{ emoji: "↖️", distance: { min: 6, max: 8 }, angle: { gt: 292.5, lt: 337.5 } },
-				{ emoji: "⬜️", distance: null, angle: null }
-			]
+			emoji: presets.simple
 		}
 	}
 
