65ba6dd3a2a7d2e181065a5ad381b8dca2768380
hd whitefur <hughdwhite@gmail.com>
2020-08-03 16:52:40 -0500

    create dummy nav and control classes with basic styling take 2

---
+++ src/Control.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class Control extends Component {
+    render() {
+        return (
+            <div className="control"><p>Controls go here!</p></div>
+        )
+    }
+}
+
+export default Control
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class Nav extends Component {
+    render() {
+        return (
+            <div className="nav"><b>Emoji Grid</b></div>
+        )
+    }
+}
+
+export default Nav
