02de76d6771cdc07859a065089ca4771a11b989e
hd whitefur <hughdwhite@gmail.com>
2020-08-07 15:44:25 -0500

    convert target image to dynamically generated canvas

--- src/Planner.js
+++ src/Planner.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import target from "./img/target.png";
 import EmojiInput from "./EmojiInput";
 import PlannerControl from "./PlannerControl";
 
@@ -8,25 +7,86 @@ class Planner extends Component {
 		super(props);
 
 		this.state = {
-			emojiInputs: []
+			emojiInputs: [],
+			shapes: {
+				arcs: [],
+				lines: []
+			}
 		}
 
 		this.updateValue = this.updateValue.bind(this);
-		this.spawnBox = this.spawnBox.bind(this);
+		this.handleClick = this.handleClick.bind(this);
 		this.processInputs = this.processInputs.bind(this);
 		this.switchMode = this.switchMode.bind(this);
+	}
+
+	generateShapes(radius0 = 40, divisions = 8) {
+		let angle0 = 180 / divisions;
+		let angleDelta = 360 / divisions
+		let getCoords = function (r, a) {
+			return {
+				x: 250 + (r * Math.cos(a * Math.PI / 180)),
+				y: 250 + (r * Math.sin(a * Math.PI / 180))
+			};
+		}
+
+		let arcs = [];
+		let lines = [];
+		for (let r = radius0; r <= 250; r += radius0) {
+			for (let a = -angle0; a < 360 - angleDelta; a += 360 / divisions) {
+				arcs.push({
+					x: 250,
+					y: 250,
+					r: r,
+					sA: a * Math.PI / 180,
+					eA: (a + angleDelta) * Math.PI / 180
+				});
+				let coords = getCoords(r, a);
+				lines.push({
+					x: coords.x,
+					y: coords.y
+				});
+			}
+		}
+		return {arcs: arcs, lines: lines};
+	}
+
+	componentDidMount() {
+		const ctx = this.refs.canvas.getContext("2d");
+		let shapes = this.generateShapes();
+
+		shapes.arcs.forEach(arc => {
+			ctx.beginPath();
+			ctx.arc(arc.x, arc.y, arc.r, arc.sA, arc.eA);
+			ctx.strokeStyle = "#000000";
+			ctx.lineWidth = 2;
+			ctx.stroke();
+		})
+		shapes.lines.forEach(line => {
+			ctx.moveTo(250, 250);
+			ctx.lineTo(line.x, line.y);
+			ctx.lineWidth = 1;
+			ctx.stroke();
+		})
 	}
 
 	render() {
 		return (
 			<div className="pair">
 				<div className="planner">
-					<img src={target} id="target" width="500" height="500" alt="a target-shaped guide" onClick={this.spawnBox}></img>
+					<canvas ref="canvas" width="500" height="500" onClick={this.handleClick}></canvas>
 					{this.state.emojiInputs.map((item) => item.element)}
 				</div>
 				<PlannerControl processInputs={this.processInputs} switchMode={this.switchMode} />
 			</div>
 		)
+	}
+
+	handleClick(e) {
+		let rect = this.refs.canvas.getBoundingClientRect();
+		let x = e.pageX - rect.x;
+		let y = e.pageY - rect.y;
+		const ctx = this.refs.canvas.getContext("2d");
 	}
 
 	//I recognize this is a messy way to do this, but
@@ -46,7 +106,7 @@ class Planner extends Component {
 	}
 
 	spawnBox(e) {
-		let rect = document.getElementById("target").getBoundingClientRect();
+		let rect = e.target.getBoundingClientRect();
 		let relCoords = { x: e.pageX - rect.x, y: e.pageY - rect.y };
 		let keyCoords = "x" + relCoords.x + "y" + relCoords.y;
 		let newEmojiInput = <EmojiInput relCoords={relCoords} key={keyCoords} index={this.state.emojiInputs.length}
@@ -91,7 +151,7 @@ class Planner extends Component {
 		});
 		emoji.push({ emoji: "⬜️", distance: null, angle: null });
 
-		return {name: "Custom", emoji: emoji};
+		return { name: "Custom", emoji: emoji };
 	}
 
 	switchMode() {
