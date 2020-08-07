918853d5fe7850ebaf6d33f98e69673143377f7c
hd whitefur <hughdwhite@gmail.com>
2020-08-07 18:42:00 -0500

    add indices for shapes

--- src/Planner.js
+++ src/Planner.js
@@ -49,6 +49,10 @@ class Planner extends Component {
 	//Generates circles interspersed by radius0 and
 	//dividing lines indicated by divisions
 	//through smaller arcs and sub-lines
+	//
+	//Shapes contain indices r (radius from center)
+	//and a (angle from center starting to right of center
+	//and incrementing by 1 moving counterclockwise)
 	generateShapes(radius0 = 40, divisions = 8) {
 		let angle0 = 180 / divisions;
 		let angleDelta = 360 / divisions
@@ -60,13 +64,13 @@ class Planner extends Component {
 				const arc = new Path2D();
 				arc.arc(250, 250, r - 1, a * Math.PI / 180, (a + angleDelta) * Math.PI / 180);
 				arc.arc(250, 250, r + 1, (a + angleDelta) * Math.PI / 180, a * Math.PI / 180, true);
-				arcs.push({ arc: arc, visible: true });
+				arcs.push({ arc: arc, r: (r / radius0) - 1, a: divisions - ((a + angle0) / angleDelta), visible: true });
 
 				if (r !== radius0) {
 					const line = new Path2D();
 					line.arc(250, 250, r - radius0, (a - 1) * Math.PI / 180, (a + 1) * Math.PI / 180);
 					line.arc(250, 250, r, (a + 1) * Math.PI / 180, (a - 1) * Math.PI / 180, true);
-					lines.push({ line: line, visible: true });
+					lines.push({ line: line, r: (r / radius0) - 1, a: divisions - ((a + angle0) / angleDelta), visible: true });
 				}
 			}
 		}
@@ -79,7 +83,8 @@ class Planner extends Component {
 		let y = e.pageY - rect.y;
 		const ctx = this.refs.canvas.getContext("2d");
 		for (let i = 0; i < this.state.shapes.arcs.length; i++) {
-			if (ctx.isPointInPath(this.state.shapes.arcs[i].arc, x, y)) {
+			if (ctx.isPointInPath(this.state.shapes.arcs[i].arc, x, y) &&
+				this.state.shapes.arcs[i].r !== 0) {
 				this.toggleArcVisibility(i);
 			}
 		}
